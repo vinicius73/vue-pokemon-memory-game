365e263838131d61830f0dbd48358f9b13b256c3
Vinicius Reis <luiz.vinicius73@gmail.com>
2019-02-26 14:21:55 -0300

    footer links

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,8 +1,11 @@
 <template lang="html">
   <footer class="footer">
-    <div class="content has-text-centered">
+    <div class="has-text-centered">
       <p>
-        @vinicius73
+        Made with
+        <b-icon color="red" type="is-success" size="is-small" icon="vuejs" />
+        by
+        <a href="https://github.com/vinicius73" target="_blank">@vinicius73</a>
       </p>
     </div>
   </footer>
@@ -13,3 +16,10 @@ export default {
   name: 'Footer'
 }
 </script>
+
+<style media="screen">
+  .footer {
+    background-color: transparent !important;
+    font-family: monospace;
+  }
+</style>

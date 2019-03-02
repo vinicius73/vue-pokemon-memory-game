2260becb907d9f2607ca9cc3776b9b8b89cee707
Vinicius Reis <luiz.vinicius73@gmail.com>
2019-03-02 15:18:48 -0300

    imgRgx and debug

--- public/precache-sw.js
+++ public/precache-sw.js
@@ -2,15 +2,22 @@ self.workbox.precaching.precache([
   'https://cdn.materialdesignicons.com/2.5.94/css/materialdesignicons.min.css'
 ])
 
+const imgRgx = new RegExp(/(https?:\/\/.*\.(?:png|jpg))/i)
+
 const handler = new self.workbox.strategies.CacheFirst({
   cacheName: 'image-cache'
 })
 
+const matchFunction = ({ url }) => {
+  console.log(url)
+  return imgRgx.test(url)
+}
+
 self.workbox.routing.registerRoute(
-  new RegExp('.+\\.png$'),
+  matchFunction,
   handler
 )
 self.workbox.routing.registerRoute(
-  new RegExp('.+\\.jpg$'),
+  matchFunction,
   handler
 )
